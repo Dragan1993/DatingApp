3cd3b00f2d52412d693e32269ffdcca1c3c1e8f5
Dragan Milovac <draganmilovac.1993@gmail.com>
2020-03-22 18:45:49 +0100

    security and login and register page

---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { error } from 'protractor';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  model: any = {};
+  @Output() cancelEmmiter = new EventEmitter();
+  constructor(private services: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.services.register(this.model).subscribe(() => {
+      console.log('Registration successfull');
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  cancel() {
+    this.cancelEmmiter.emit(false);
+    console.log('cancel');
+  }
+}

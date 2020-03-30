2d2989bdd33e9eb9b8943be613b71e6a70cc1754
Dragan Milovac <draganmilovac.1993@gmail.com>
2020-03-30 23:11:51 +0200

    Extend API with Photos

---
+++ DatingApp.API/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IDatingRepository repo;
+        private IMapper mapper;
+
+        public UsersController(IDatingRepository repo)
+        {
+            this.repo = repo;
+            this.mapper=mapper;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await repo.GetUser(id);
+            var userToReturn = mapper.Map<UserForDetailedDto>(user);
+            return Ok(userToReturn);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await repo.GetUsers();
+            var usersToReturn = mapper.Map<IEnumerable<UserForListDto>>(users);
+            return Ok(usersToReturn);
+        }
+    }
+}
